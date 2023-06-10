bb98345d2da8a667bb898842b4c81ccb5e548bc2
CianMW <88888516+CianMW@users.noreply.github.com>
2023-06-10 22:20:33 +0100

    Modded for build stability
    - removed generateEncounter and realmCrawl data

--- src/utils/generateEncounter.ts
+++ src/utils/generateEncounter.ts
@@ -1,15 +1,15 @@
 
-export default function generateEncounter(pcNumber, environment, level){
-let encounterLevel 
-if(pcNumber > 1){
-    encounterLevel= getEncounterLevel(convertPcLevel(level))
-} else {
-    encounterLevel = getEncounterLevel(level)
-}
+// export default function generateEncounter(pcNumber:number, environment:string, level:number){
+// let encounterLevel 
+// if(pcNumber > 1){
+//     encounterLevel= getEncounterLevel(convertPcLevel(level))
+// } else {
+//     encounterLevel = getEncounterLevel(level)
+// }
 
-return encounterLevel.forEach(cr => {
-    let listLength = Data[environment][cr].listLength;
-    let monster = Data[environment][cr][Math.floor(Math.random()* listLength)]
-    return monster
-});
-}
+// return encounterLevel.forEach(cr => {
+//     let listLength = Data[environment][cr].listLength;
+//     let monster = Data[environment][cr][Math.floor(Math.random()* listLength)]
+//     return monster
+// });
+// }
